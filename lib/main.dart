775c218f4a54b02a7d4cd775a914613ecd812f60
Aguidi Tayo William <wtaguidi@gmail.com>
2026-05-25 19:53:42 +0100

    corrections on previous emailVerification

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:inventory/Views/login_view.dart';
 import 'package:inventory/Views/register_view.dart';
+import 'package:inventory/Views/verify_email.dart';
 import 'package:inventory/firebase_options.dart';
 
 void main() {
@@ -34,13 +35,18 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            //final user = FirebaseAuth.instance.currentUser;
-            //if (user?.emailVerified ?? false) {
-            //return const Text('Done');
-            //} else {
-            //return const VerifyEmailView();
-            //}
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is not verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
+
           default:
             return const Center(child: CircularProgressIndicator());
         }
